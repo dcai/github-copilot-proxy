d26185631c16fef9885a2eb49d066f1b5baaf2e8
dcai <d@tux.im>
2025-04-24 17:39:02 +1000

    fix(app.js): update server port and host; improve stream data handling and logging

--- app.js
+++ app.js
@@ -9,11 +9,12 @@ const {
 } = require("./config");
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.GHC_PORT || 7890;
+const host = process.env.GHC_HOST || "0.0.0.0";
 
 app.use(express.json());
 
-app.get("/v1/models", async (req, res) => {
+app.get("/v1/models", async (_req, res) => {
   const copilotToken = await tokenManager.getToken();
   const options = {
     method: "GET",
@@ -67,8 +68,80 @@ app.post("/v1/chat/completions", async (req, res) => {
       });
       res.flushHeaders();
 
+      let buffer = "";
       dataStream.on("data", (chunk) => {
-        console.info("OpenAI stream chunk:", chunk.toString());
+        buffer += chunk;
+
+        const lines = buffer.split("\n");
+        buffer = lines.pop(); // Keep the last line in the buffer
+
+        for (let line of lines) {
+          line = line.trim();
+          if (line.includes("[DONE]")) {
+            continue;
+          }
+          try {
+            if (!line) {
+              continue;
+            }
+
+            // {
+            //   "choices": [
+            //     {
+            //       "finish_reason": "stop",
+            //       "index": 0,
+            //       "delta": {
+            //         "content": null,
+            //         "annotations": {
+            //           "CodeVulnerability": [
+            //             {
+            //               "id": 0,
+            //               "start_offset": 48,
+            //               "end_offset": 48,
+            //               "details": { "type": "general" },
+            //               "citations": {},
+            //             },
+            //           ],
+            //         },
+            //         "copilot_annotations": {
+            //           "CodeVulnerability": [
+            //             {
+            //               "id": 0,
+            //               "start_offset": 48,
+            //               "end_offset": 48,
+            //               "details": { "type": "general" },
+            //               "citations": {},
+            //             },
+            //           ],
+            //         },
+            //       },
+            //     },
+            //   ],
+            //   "created": 1745479088,
+            //   "id": "",
+            //   "usage": {
+            //     "completion_tokens": 305,
+            //     "prompt_tokens": 10,
+            //     "total_tokens": 315,
+            //   },
+            //   "model": "gpt-4.1-2025-04-14",
+            //   "system_fingerprint": "fp_11111",
+            // }
+            if (line.startsWith("data:")) {
+              const str = line.replace(/^data:\s*/, "");
+              const json = JSON.parse(str);
+              const stopped = json?.choices?.[0]?.finish_reason === "stop";
+              if (stopped) {
+                console.info(`model: ${json.model}`);
+                console.info(`usage: ${json?.usage?.total_tokens}`);
+              }
+            }
+          } catch (ex) {
+            // console.error(ex);
+            console.error(ex.toString(), " => ", str);
+          }
+        }
+
         res.write(chunk);
       });
 
@@ -89,5 +162,5 @@ app.post("/v1/chat/completions", async (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Copilot proxy listening on http://localhost:${port}`);
+  console.log(`Copilot Chat Proxy listening on http://${host}:${port}`);
 });
