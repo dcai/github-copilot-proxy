973116c7b901df744d68972e1ab4d4e16284171d
dcai <d@tux.im>
2025-04-24 16:42:08 +1000

    initial commit

---
+++ app.js
@@ -0,0 +1,80 @@
+require("dotenv").config();
+const express = require("express");
+const axios = require("axios");
+const tokenManager = require("./token");
+const {
+  EDITOR_VERSION,
+  USER_AGENT,
+  EDITOR_PLUGIN_VERSION,
+} = require("./config");
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+// Proxy endpoint
+app.post("/v1/chat/completions", async (req, res) => {
+  try {
+    const payload = req.body;
+    const copilotToken = await tokenManager.getToken();
+
+    const stream = payload?.stream || false;
+    const options = {
+      method: "POST",
+      url: "https://api.githubcopilot.com/chat/completions",
+      headers: {
+        authorization: `Bearer ${copilotToken}`,
+        "content-type": "application/json",
+        "editor-version": EDITOR_VERSION,
+        "user-agent": USER_AGENT,
+        "editor-plugin-version": EDITOR_PLUGIN_VERSION,
+      },
+      data: payload,
+    };
+    if (!stream) {
+      const response = await axios.request(options);
+      return res.json(response.data);
+    }
+
+    if (stream) {
+      const response = await axios.request({
+        ...options,
+        responseType: "stream",
+      });
+      const dataStream = response.data;
+
+      res.writeHead(200, {
+        "Content-Type": "text/event-stream",
+        "Cache-Control": "no-cache",
+        Connection: "keep-alive",
+      });
+      res.flushHeaders();
+
+      dataStream.on("data", (chunk) => {
+        console.info("OpenAI stream chunk:", chunk.toString());
+        res.write(chunk);
+      });
+
+      dataStream.on("end", () => {
+        // res.write("\n\n");
+        res.end();
+      });
+
+      dataStream.on("error", (err) => {
+        console.error("OpenAI stream error:", err);
+        res.end();
+      });
+    }
+  } catch (err) {
+    if (err.response) {
+      res.status(err.response.status).json(err.response.data);
+    } else {
+      res.status(500).json({ error: err.message });
+    }
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Copilot proxy listening on http://localhost:${port}`);
+});
---
+++ config.js
@@ -0,0 +1,3 @@
+export const EDITOR_VERSION = "vscode/1.85.1";
+export const EDITOR_PLUGIN_VERSION = "copilot-chat/0.17.2024062801";
+export const USER_AGENT = "GitHubCopilotChat/0.17.2024062801";
