7713c3d63b6a85608664f5fcea53d0a36b5b5aaa
dcai <d@tux.im>
2025-05-01 13:13:21 +1000

    feat: Implement device code flow for GitHub Copilot authentication
    This commit introduces a script to obtain an access token for GitHub Copilot using the device code flow. It retrieves the device code, prompts the user to enter the code on GitHub, and then polls for the access token until it is granted. The script uses environment variable `MY_COPILOT_CLIENT_ID` for client ID.

---
+++ setup.js
@@ -0,0 +1,76 @@
+const clientId = process.env.MY_COPILOT_CLIENT_ID;
+
+if (!clientId) {
+  console.error("MY_COPILOT_CLIENT_ID is not set");
+  process.exit(1);
+}
+
+const commonHeaders = new Headers();
+commonHeaders.append("accept", "application/json");
+commonHeaders.append("editor-version", "Neovim/0.6.1");
+commonHeaders.append("editor-plugin-version", "copilot.vim/1.16.0");
+commonHeaders.append("content-type", "application/json");
+commonHeaders.append("user-agent", "GithubCopilot/1.155.0");
+commonHeaders.append("accept-encoding", "gzip,deflate,b");
+
+async function getDeviceCode() {
+  const raw = JSON.stringify({
+    "client_id": clientId,
+    "scope": "read:user",
+  });
+
+  const requestOptions = {
+    method: "POST",
+    headers: commonHeaders,
+    body: raw,
+    redirect: "follow",
+  };
+
+  const data = await fetch(
+    "https://github.com/login/device/code",
+    requestOptions,
+  )
+    .then((response) => response.json())
+    .catch((error) => console.error(error));
+  return data;
+}
+
+async function getAccessToken(deviceCode) {
+  const raw = JSON.stringify({
+    "client_id": clientId,
+    "device_code": deviceCode,
+    "grant_type": "urn:ietf:params:oauth:grant-type:device_code",
+  });
+
+  const requestOptions = {
+    method: "POST",
+    headers: commonHeaders,
+    body: raw,
+    redirect: "follow",
+  };
+
+  return await fetch(
+    "https://github.com/login/oauth/access_token",
+    requestOptions,
+  )
+    .then((response) => response.json())
+    .catch((error) => console.error(error));
+}
+
+(async function () {
+  const { device_code, user_code, verification_uri, expires_in } =
+    await getDeviceCode();
+  console.info(`enter user code:\n${user_code}\n${verification_uri}`);
+  console.info(`Expires in ${expires_in} seconds`);
+  while (true) {
+    await new Promise((resolve) => setTimeout(resolve, 5000));
+    const accessToken = await getAccessToken(device_code);
+    if (accessToken.error) {
+      console.info(`${accessToken.error}: ${accessToken.error_description}`);
+    }
+    if (accessToken.access_token) {
+      console.info(`access token:\n${JSON.stringify(accessToken, null, 1)}`);
+      break;
+    }
+  }
+})();
